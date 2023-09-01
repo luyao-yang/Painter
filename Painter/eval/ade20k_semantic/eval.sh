5d4027ef30a6ed4d669bc779409c86e754670e54
luyao-yang <luyaoyang98@gmail.com>
2023-09-01 18:18:48 +0300

    update

--- Painter/eval/ade20k_semantic/eval.sh
+++ Painter/eval/ade20k_semantic/eval.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-NUM_GPUS=4
+NUM_GPUS=1
 JOB_NAME="painter_vit_large"
 CKPT_FILE="painter_vit_large.pth"
 PROMPT=ADE_train_00009574
@@ -10,8 +10,10 @@ PROMPT=ADE_train_00009574
 SIZE=448
 MODEL="painter_vit_large_patch16_input896x448_win_dec64_8glb_sl1"
 
-CKPT_PATH="models/${JOB_NAME}/${CKPT_FILE}"
-DST_DIR="models_inference/${JOB_NAME}/ade20k_semseg_inference_${CKPT_FILE}_${PROMPT}_size${SIZE}"
+# CKPT_PATH="models/${JOB_NAME}/${CKPT_FILE}"
+CKPT_PATH="pretrained/${CKPT_FILE}"
+# DST_DIR="models_inference/${JOB_NAME}/ade20k_semseg_inference_${CKPT_FILE}_${PROMPT}_size${SIZE}"
+DST_DIR="models_inference/pretrained/ade20k_semseg_inference_${CKPT_FILE}_${PROMPT}_size${SIZE}"
 
 # inference
 python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} --master_port=29504 --use_env \
